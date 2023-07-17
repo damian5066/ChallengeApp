20de79d3b85d77d2ff877e891a6e5b1fca7c9378
damian5066 <damian5066@wp.pl>
2023-07-18 01:28:32 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Damian";
+bool isWoman = false;
+var age = 15;
+
+if (isWoman == false)
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
+else
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta powyżej 29lat");
+    }
+}
