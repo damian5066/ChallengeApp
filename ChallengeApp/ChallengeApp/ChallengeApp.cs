ec5fda44f850fc0e1fec3d1accffd1fccadf9b7a
damian5066 <damian5066@wp.pl>
2023-07-19 18:07:24 +0200

    Ostatenie zmiany zadanie6

---
+++ ChallengeApp/ChallengeApp/ChallengeApp.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ChallengeApp
+{
+    class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Sum
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void addScore(int points)
+        {
+            this.score.Add(points);
+        }
+    }
+
+
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,69 +1,41 @@
-﻿
-Employee Employee1 = new Employee("Marek", "Chodny", 20); 
-Employee Employee2 = new Employee("Julia", "Mak", 33);
-Employee Employee3 = new Employee("Damian", "Sokolik", 35);
-Employee1.addScore(2);
-Employee1.addScore(4);
-Employee1.addScore(6);
-Employee1.addScore(7);
-Employee1.addScore(9);
+﻿using ChallengeApp;
 
-Employee2.addScore(2);
-Employee2.addScore(2);
-Employee2.addScore(2);
-Employee2.addScore(2);
-Employee2.addScore(2);
+Employee employee1 = new Employee("Marek", "Chodny", 20); 
+Employee employee2 = new Employee("Julia", "Mak", 33);
+Employee employee3 = new Employee("Damian", "Sokolik", 35);
+employee1.addScore(2);
+employee1.addScore(4);
+employee1.addScore(6);
+employee1.addScore(7);
+employee1.addScore(9);
 
-Employee3.addScore(9);
-Employee3.addScore(9);
-Employee3.addScore(9);
-Employee3.addScore(8);
-Employee3.addScore(1);
+employee2.addScore(2);
+employee2.addScore(2);
+employee2.addScore(2);
+employee2.addScore(2);
+employee2.addScore(2);
+
+employee3.addScore(9);
+employee3.addScore(9);
+employee3.addScore(9);
+employee3.addScore(8);
+employee3.addScore(1);
 
 
 List<Employee> employees = new List<Employee>()
 {
-    Employee1, Employee2, Employee3
+    employee1, employee2, employee3
 };
 
 int maxScore = -1;
-Employee EwithMaxScore = null;
+Employee employeeWithMaxScore = null;
 
 foreach (var employee in employees)
 {
     if (employee.Sum > maxScore)
     {
-        EwithMaxScore = employee;
+        employeeWithMaxScore = employee;
     }
 }
-Console.WriteLine(EwithMaxScore.Name + " " + EwithMaxScore.Surname + " " + EwithMaxScore.Age + "lat, Uzyskany wynik: " + EwithMaxScore.Sum);
-
-class Employee
-{
-    private List<int> score = new List<int>();
-
-    public Employee(string name, string surname, int age)
-    {
-        this.Name = name;
-        this.Surname = surname;
-        this.Age = age;
-    }
-
-    public string Name { get; private set; }
-    public string Surname { get; private set; }
-    public int Age { get; private set; }
-
-    public int Sum
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-
-    public void addScore(int points)
-    {
-        this.score.Add(points);
-    }
-}
+Console.WriteLine(employeeWithMaxScore.Name + " " + employeeWithMaxScore.Surname + " " + employeeWithMaxScore.Age + "lat, Uzyskany wynik: " + employeeWithMaxScore.Sum);
 
